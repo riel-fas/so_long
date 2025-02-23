858eb74b19be6c69a40c5181416348b861bc6336
Riad El Fassi <riel-fas@c2r4p10.1337.ma>
2025-02-23 06:21:19 +0000

    so_longv1

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: riel-fas <riel-fas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 01:19:52 by riel-fas          #+#    #+#             */
-/*   Updated: 2025/02/23 02:35:18 by riel-fas         ###   ########.fr       */
+/*   Updated: 2025/02/23 06:18:43 by riel-fas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,37 @@
 
 int main()
 {
+t_game game;
 
-	t_game game;
+// Initialize MLX and create a window
+game.mlx = mlx_init(800, 600, "so_long", true);
+if (!game.mlx)
+    return (EXIT_FAILURE);
 
-	//MLX inis. and window opening
-	game.mlx = mlx_init(1000, 1000, "the fucking_window", true);
-	if (!game.mlx)
-		return (EXIT_FAILURE);
+// // Initialize the game state (hardcoded for now)
+// game.map = (char *[]) {
+//     "11111",
+//     "10P01",
+//     "1C0E1",
+//     "11111",
+//     NULL
+// };
+// game.map_width = 5;
+// game.map_height = 4;
+// game.player_x = 2; // Initial player position
+// game.player_y = 1;
+// game.moves = 0;    // Initialize move counter
 
+// Register the key hook
+mlx_key_hook(game.mlx, keyboard, &game);
 
+// Draw the initial map
+// draw_map(&game);
 
-	//key hook
-	mlx_key_hook(game.mlx, keyboard, &game);
+// Start the MLX event loop
+mlx_loop(game.mlx);
 
-
-
-	//MLX event loop
-	mlx_loop(game.mlx);
-
-
-
-
-
-
-	//Clean and exit
-	mlx_terminate(game.mlx);
-
-	return (EXIT_SUCCESS);
+// Clean up and exit
+mlx_terminate(game.mlx);
+return (EXIT_SUCCESS);
 }
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: riel-fas <riel-fas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 01:16:15 by riel-fas          #+#    #+#             */
-/*   Updated: 2025/02/23 02:33:10 by riel-fas         ###   ########.fr       */
+/*   Updated: 2025/02/23 06:19:03 by riel-fas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,23 @@
 
 # include "/Users/riel-fas/MLX42/include/MLX42/MLX42.h"
 # include <stdlib.h>
+# include <stdio.h>
+
+#define TILE_SIZE 32  // or whatever size you want your tiles to be
 
 
-typedef	struct	s_game
+typedef struct s_game
 {
-	mlx_t	*mlx;
-	char	**map;
-	int		map_width;
-	int		map_height;
-	int		player_x;
-	int		player_y;
+    mlx_t   *mlx;
+    char    **map;
+    int     map_width;
+    int     map_height;
+    int     player_x;
+    int     player_y;
+    int     moves;
+} t_game;
 
-}	t_game;
-
-void	keyboard(mlx_key_data_t keydata, void *param);
-
-
-
-
-
-
-
-
-
-
+void    keyboard(mlx_key_data_t keydata, void *param);
+void draw_map(t_game *game);
 
 #endif
